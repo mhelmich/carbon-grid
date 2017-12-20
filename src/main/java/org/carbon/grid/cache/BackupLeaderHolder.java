460a6035b825ca13c7da32a21a64764eaf5aa093
mhelmich <marco.helmich@live.com>
2017-12-19 18:44:41 -0800

    - adding test coverage - managing reference count of buffers better

--- src/main/java/org/carbon/grid/cache/BackupLeaderHolder.java
+++ src/main/java/org/carbon/grid/cache/BackupLeaderHolder.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2017 Marco Helmich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.carbon.grid.cache;
 
 import com.google.common.collect.ImmutableMap;
 import org.cliffc.high_scale_lib.NonBlockingHashMapLong;
 
 import java.io.Closeable;
-import java.io.IOException;
 import java.util.Map;
 
 class BackupLeaderHolder implements Closeable {
@@ -21,20 +36,33 @@ class BackupLeaderHolder implements Closeable {
     }
 
     synchronized void put(long newLeaderEpoch, CacheLine line) {
-        if (leaderEpoch <= newLeaderEpoch) {
+        if (leaderEpoch < newLeaderEpoch) {
             leaderEpoch = newLeaderEpoch;
             CacheLine oldLine = backUp.get(line.getId());
             if (oldLine == null) {
                 backUp.put(line.getId(), line);
             } else {
                 if (oldLine.getVersion() < line.getVersion()) {
+                    // release the buffer so that we don't have worry about that anywhere else
+                    oldLine.releaseData();
                     backUp.put(line.getId(), line);
+                } else {
+                    // destroy the buffer since we don't want to take care of this
+                    // higher up the stack
+                    line.releaseData();
                 }
             }
+        } else {
+            // destroy the buffer since we don't want to take care of this
+            // higher up the stack
+            line.releaseData();
         }
     }
 
     Map<Long, CacheLine> getBackup() {
+        // defensive copy
+        // keep in mind that we don't increase the reference count on the buffers
+        // so technically the backup holder still owns them
         return ImmutableMap.copyOf(backUp);
     }
 
@@ -44,7 +72,7 @@ class BackupLeaderHolder implements Closeable {
     }
 
     @Override
-    public void close() throws IOException {
+    public void close() {
         backUp.forEach((id, line) -> line.releaseData());
     }
 }
--- src/test/java/org/carbon/grid/cache/BackupImplTest.java
+++ src/test/java/org/carbon/grid/cache/BackupImplTest.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2017 Marco Helmich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.carbon.grid.cache;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import org.carbon.grid.BaseTest;
 import org.junit.Test;
 
@@ -19,23 +36,55 @@ public class BackupImplTest extends BaseTest {
         assertEquals(1, backup.getCacheLinesForLeader(leaderNodeId).size());
         assertEquals(line.getVersion(), backup.getCacheLinesForLeader(leaderNodeId).get(line.getId()).getVersion());
 
-        CacheLine line2 = newRandomCacheLine();
+        // shouldn't update because of lower epoch
+        CacheLine line2 = newRandomCacheLine(line.getId());
         line2.setVersion(line.getVersion() - 10);
-        backup.backUp(leaderNodeId, leaderEpoch - 7, line);
+        backup.backUp(leaderNodeId, leaderEpoch - 7, line2);
         assertEquals(leaderEpoch, backup.getLeaderEpochFor(leaderNodeId));
         assertEquals(1, backup.getCacheLinesForLeader(leaderNodeId).size());
         assertEquals(line.getVersion(), backup.getCacheLinesForLeader(leaderNodeId).get(line.getId()).getVersion());
+        assertEquals(Unpooled.EMPTY_BUFFER, line2.resetReaderAndGetReadOnlyData());
+
+        // shouldn't update because of lower version
+        CacheLine line3 = newRandomCacheLine(line.getId());
+        line3.setVersion(line.getVersion() - 10);
+        Long newLeaderEpoch = leaderEpoch + 7;
+        backup.backUp(leaderNodeId, newLeaderEpoch, line3);
+        assertEquals(newLeaderEpoch, backup.getLeaderEpochFor(leaderNodeId));
+        assertEquals(1, backup.getCacheLinesForLeader(leaderNodeId).size());
+        assertEquals(line.getVersion(), backup.getCacheLinesForLeader(leaderNodeId).get(line.getId()).getVersion());
+        assertEquals(Unpooled.EMPTY_BUFFER, line3.resetReaderAndGetReadOnlyData());
+
+        // updates!!!
+        CacheLine line4 = newRandomCacheLine(line.getId());
+        int newVersion = line.getVersion() + 10;
+        line4.setVersion(newVersion);
+        newLeaderEpoch++;
+        backup.backUp(leaderNodeId, newLeaderEpoch, line4);
+        assertEquals(newLeaderEpoch, backup.getLeaderEpochFor(leaderNodeId));
+        assertEquals(1, backup.getCacheLinesForLeader(leaderNodeId).size());
+        assertEquals(newVersion, backup.getCacheLinesForLeader(leaderNodeId).get(line.getId()).getVersion());
+        assertEquals(1, line4.resetReaderAndGetReadOnlyData().refCnt());
+        // old buffer has been released
+        assertEquals(Unpooled.EMPTY_BUFFER, line.resetReaderAndGetReadOnlyData());
+
+        backup.stopBackupFor(leaderNodeId);
+        assertEquals(0, backup.getCacheLinesForLeader(leaderNodeId).size());
     }
 
-    private CacheLine newRandomCacheLine() {
+    private CacheLine newRandomCacheLine(long id) {
         ByteBuf buffer = newRandomBuffer();
 
         return new CacheLine(
-                random.nextLong(),
+                id,
                 random.nextInt(10000),
                 leaderNodeId,
                 CacheLineState.INVALID,
                 buffer
         );
+    }
+
+    private CacheLine newRandomCacheLine() {
+        return newRandomCacheLine(random.nextLong());
     }
 }
